d0a87c05dc02398cdc982b6e58361213f27c0067
BasselAmgad <bamgad7@gmail.com>
2022-07-07 13:48:31 +0200

    - List initialization

--- Client/Models/Class.cs
+++ Client/Models/Class.cs
@@ -4,7 +4,7 @@
     public string Title { get; set; }
     public string Ingredients { get; set; }
     public string Instructions { get; set; }
-    public List<String> Categories { get; set; }
+    public List<String> Categories { get; set; } = new();
 
     public Recipe(string title, string ingredients, string instructions, List<String> categories)
     {
